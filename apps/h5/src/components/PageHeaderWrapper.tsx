3e4221a8fdf410b8fe6c44187f4bb070d097f427
Your Name <you@example.com>
2025-06-10 17:42:10 +0800

    feat(apps/h5): 屏幕滚动时自动收起/展开导航头

--- apps/h5/src/components/PageHeaderWrapper.tsx
+++ apps/h5/src/components/PageHeaderWrapper.tsx
@@ -1,6 +1,8 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useEffect, useRef, useState } from 'react';
 import { NavBar } from 'antd-mobile';
 import { NavBarProps } from 'antd-mobile/es/components/nav-bar';
+import { throttle } from 'lodash-es';
+import { cn } from '@/utils/utils';
 
 interface PageHeaderWrapperProps extends NavBarProps {
   title?: string;
@@ -8,12 +10,39 @@ interface PageHeaderWrapperProps extends NavBarProps {
 }
 
 const PageHeaderWrapper: FC<PageHeaderWrapperProps> = ({ title, children, ...restProps }) => {
+  const [hide, setHide] = useState(false);
+  const lastScrollY = useRef(0);
+
+  useEffect(() => {
+    const handleScroll = throttle(() => {
+      if (window.scrollY - lastScrollY.current > 60) {
+        setHide(true);
+        lastScrollY.current = window.scrollY;
+      } else if (window.scrollY - lastScrollY.current < -60) {
+        setHide(false);
+        lastScrollY.current = window.scrollY;
+      }
+      console.log(window.scrollY);
+    }, 300);
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div>
-      <NavBar style={{ borderBottom: '1px solid #444' }} {...restProps}>
-        {title}
-      </NavBar>
-      <div className="p-4">{children}</div>
+      <div
+        className={cn(
+          'border-b-1 border-b-neutral-700 fixed left-0 right-0 top-0 bg-neutral-900 z-10',
+          'transition-transform duration-200 ease-linear',
+          { '-translate-y-full': hide },
+          'backdrop-blur-xl bg-[#0f0f0f]/70',
+        )}
+      >
+        <NavBar {...restProps}>{title}</NavBar>
+      </div>
+      <div className="p-4 pt-15">{children}</div>
     </div>
   );
 };
