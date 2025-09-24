8ed2443981c9476d7cf02de739ce2e07e15fa540
sujingclg <sujingclg@gmail.com>
2025-09-25 00:16:12 +0800

    feat(apps/console): 转换对话框中添加原生 ffmpeg 命令

--- apps/console/src/routes/download/files/-components/convert-modal.tsx
+++ apps/console/src/routes/download/files/-components/convert-modal.tsx
@@ -2,10 +2,20 @@ import { Alert, Modal, Typography } from 'antd';
 import NiceModal, { useModal } from '@ebay/nice-modal-react';
 import { CodeHighlighter } from '@/components/code-highlighter';
 
-export const ConvertModal = NiceModal.create(({ path }: { path: string }) => {
+export const ConvertModal = NiceModal.create(({ path }: { path?: string }) => {
   const modal = useModal();
 
-  const cmd = `docker run --rm -it \\
+  const cmd = `sudo ffmpeg \\
+-hwaccel cuda \\
+-hwaccel_output_format cuda \\
+-i ${path} \\
+-vf scale_cuda=640:360 \\
+-c:v h264_nvenc \\
+-b:v 512K \\
+-max_muxing_queue_size 1024 \\
+-y ${path?.slice(0, path.lastIndexOf('.'))}_360p.mp4`;
+
+  const dockerCmd = `docker run --rm -it \\
 --runtime=nvidia \\
 -v $(pwd)/data:/data \\
 linuxserver/ffmpeg:6.1.1 \\
@@ -16,7 +26,7 @@ linuxserver/ffmpeg:6.1.1 \\
 -c:v h264_nvenc \\
 -b:v 512K \\
 -max_muxing_queue_size 1024 \\
--y ${path.slice(0, path.lastIndexOf('.'))}_360p.mp4`;
+-y ${path?.slice(0, path.lastIndexOf('.'))}_360p.mp4`;
 
   return (
     <Modal
@@ -26,12 +36,19 @@ linuxserver/ffmpeg:6.1.1 \\
       onCancel={() => modal.hide()}
       afterClose={() => modal.remove()}
     >
-      <div className="my-4">
-        <Alert message="将以下命令拷贝到目标主机上执行" type="info" showIcon />
+      <div className="mt-4">
+        <Alert message="将以下命令拷贝到目标主机上执行" type="info" />
       </div>
       <div className="relative flex overflow-auto">
         <Typography.Paragraph copyable={{ text: cmd }} className="absolute top-5 right-5" />
         <CodeHighlighter code={cmd} lang="bash" className="flex-1 w-0" />
+      </div>
+      <div className="mt-4">
+        <Alert message="或者借助 docker 容器执行转换" type="info" />
+      </div>
+      <div className="relative flex overflow-auto">
+        <Typography.Paragraph copyable={{ text: dockerCmd }} className="absolute top-5 right-5" />
+        <CodeHighlighter code={dockerCmd} lang="bash" className="flex-1 w-0" />
       </div>
     </Modal>
   );
